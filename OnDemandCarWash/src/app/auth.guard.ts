a23a2ab4361cf2fd78292354c22484cfa92b6f58
sreechen <chennasreeja1@gmail.com>
2023-02-22 11:26:35 +0530

    OnDmandCarWash

---
+++ OnDemandCarWash/src/app/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {  CanActivate, Router} from '@angular/router';
+
+import { AuthenticationService} from './service/authentication.service'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+ constructor (private _authService:AuthenticationService, private _router:Router){}
+
+ canActivate(): boolean{
+  if(this._authService.loggedIn()){
+    return true
+  }
+
+  else{
+    this._router.navigate(['login'])
+    return false
+  }
+  }
+ }
+
+
